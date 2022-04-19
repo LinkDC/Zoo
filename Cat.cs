e9b4ce5d92acf069cef1af47e629741d0e410077
linkdcz <tim.vikulov@gmail.com>
2022-04-19 17:53:11 +0300

    Добавьте файлы проекта.

---
+++ Cat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    internal class Cat
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+        public Cat()
+        {
+            Weight = 5;
+            Name = "Мурзик";
+        }
+        public Cat(int _weight, string _name)
+        {
+            Weight = _weight;
+            Name = _name;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Кота зовут {Name} и он весит {Weight}");
+        }
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} сказал Мяу") ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zoo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Cat Tom = new Cat();
+            Cat Jerry = new Cat(4,"Джерри");
+
+            Tom.PrintInfo();
+            Tom.Meow();
+            Jerry.PrintInfo();
+            Jerry.Meow();
+
+        }
+    }
+}
